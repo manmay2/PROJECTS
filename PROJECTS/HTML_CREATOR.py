a1ce24213ab5f5518ef3a8cb6640d0b92b79a69d
Manmay <manmaychakarborty@gmail.com>
2023-03-17 20:23:53 +0530

    HTML_CREATOR

--- PROJECTS/HTML_CREATOR.py
+++ PROJECTS/HTML_CREATOR.py
@@ -1,78 +1,92 @@
 # WELCOME TO THE HTML CREATOR WHICH WILL HELP US TO GENERATE CODE FOR HTML AND CSS
-# file_name=input("ENTER THE HTML FILE NAME : ")
 
-# p_=input("DO YOU WANT TO ADD PARAGRAPH TAG : Y/N : ")
-# img_=input("DO YOU WANT TO ADD IMAGE TAG : Y/N : ")
-# H_=input("DO YOU WANT TO ADD HEADER  TAG : Y/N : ")
-# sup_=input("DO YOU WANT TO ADD SUPERSCRIPT TAG : Y/N : ")
-# sub_=input("DO YOU WANT TO ADD SUBSCRIPT TAG : Y/N : ")
-# ol_=input("DO YOU WANT TO ADD ORDER LISTS : Y/N : ")
-# ul_=input("DO YOU WANT TO ADD UNORDER LISTS : Y/N : ")
+lorem=''
 
-def p_(bas,ini):
-	add=['<p>','Sample Text Starts here............','</p>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<p>'
+def insert(bas,add,ini):
+	add=add[::-1]
+	for i in add:
+
+		bas.insert(bas.index(ini)+1,i)
+
+def p_(bas,ini):	
+	add=['<p>\n','Sample Text Starts here............','</p>\n']
+	insert(bas,add,ini)
+	ini='</p>\n'
 def img_(bas,ini):
 	# src=input("ENTER THE IMAGE'S FILENAME : ")
 	# height=int(input("ENTER THE HEIGHT OF THE IMAGE : "))
 	# width=int(input("ENTER THE WIDTH OF THE IMAGE : "))
 	# alt=input("ENTER THE ALTERNATING MESSAGE INCASE IF IMAGE ISN'T DISPLAYED")
-	add=["<img src=xyz.jpg,height=123,width=123,alt='SAMPLE GOES HERE.....'"]
-	bas.insert(bas.index(ini)+1,add)
-	ini=add
-def h1_(bas,ini):
-	add=['<h1>','Sample Text Starts here............','</h1>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h1>'
-def h2_(bas,ini):
-	add=['<h2>','Sample Text Starts here............','</h2>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h2>'
-def h3_(bas,ini):
-	add=['<h3>','Sample Text Starts here............','</h3>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h3>'
-def h4_(bas,ini):
-	add=['<h4>','Sample Text Starts here............','</h4>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h4>'
-def h5_(bas,ini):
-	add=['<h5>','Sample Text Starts here............','</h5>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h5>'
-def h6_(bas,ini):
-	add=['<h6>','Sample Text Starts here............','</h6>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<h6>'
-def sup_(bas,ini):
-	add=['<sup>','Sample Text Starts here............','</sup>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<sup>'
-def sub_(bas,ini):
-	add=['<sub>','Sample Text Starts here............','</sub>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<sub>'
-def ol_(bas,ini):
-	add=['<ol type=' '>','</ol>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<ol type=' '>'
-def li_(bas,ini):
-	add=['<li>     ','<li>      ']
-	bas.insert(bas.index(ini)+1,add)
-	ini='' 																										# DOUBT
-def ul_(bas,ini):
-	add=['<ul style=' '>','</ul>']
-	bas.insert(bas.index(ini)+1,add)
-	ini='<ul style=' '>'
-def main(bas,file_name,ini):
-	with open('INDEX.HTML','w') as f1:
-		#basic doctype
-		bas=['<!DOCTYPE html>','<html lang="en">','<head>','    <meta charset="UTF-8">',
-		'    <meta http-equiv="X-UA-Compatible" content="IE=edge">',
-		'    <meta name="viewport" content="width=device-width, initial-scale=1.0">','    <title>Document</title>','</head>',
-		'<body>','</body>','</html>']
-		for i in bas:
-			f1.writelines(i+'\n')
+	add=["<img src=xyz.jpg,height=123,width=123,alt='SAMPLE GOES HERE.....'>\n"]
+	insert(bas,add,ini)
+	ini=add[0]
+def hx_(bas,ini):
+	# x=input("ENTER THE HEADING TAG FROM 1-6 : ")
+
+	add=['<h1>\n',lorem,'</h1>\n']
+	insert(bas,add,ini)
+	ini='</h1>\n'
+
+def sup_(bas,ini,sup):
+	if(sup=='<sub>'):
+		add=['<sub>\n','Sample Text Starts here............\n','</sub>\n']
+		insert(bas,add,ini)
+		ini='</sub>\n'
+	elif(sup=='<sup>'):
+		add=['<sup>\n','Sample Text Starts here............\n','</sup>\n']
+		insert(bas,add,ini)
+		ini='</sup>\n'
 		
-	
+			
+
+def ol_(bas,ini):
+	add=['<ol type=' '>\n','</ol>\n']
+	insert(bas,add,ini)
+	ini='<ol type=' '>\n'
+def li_(bas,ini):
+	add=['<li>\n',lorem,'\n','</li>\n']
+	insert(bas,add,ini)																								
+def ul_(bas,ini):
+	add=['<ul style=' '>\n','</ul>\n']
+	insert(bas,add,ini)
+	ini='<ul style=' '>\n'
+def main():
+	title_=input("ENTER THE TITLE OF THE DOCUMENT : ")
+	file_name=input("ENTER THE FILENAME : ")
+	global lorem
+	with open(file_name,'w') as f1:
+		p=input("DO YOU WANT TO ADD PARAGRAPH TAG : Y/N : ")
+		img=input("DO YOU WANT TO ADD IMAGE TAG : Y/N : ")
+		H=input("DO YOU WANT TO ADD HEADER  TAG : Y/N : ")
+		sup=input("DO YOU WANT TO ADD SUPERSCRIPT/SUBSCRIPT TAG , enter the tag name in the tag format : ")
+		ol=input("DO YOU WANT TO ADD ORDER LISTS : Y/N : ")
+		ul=input("DO YOU WANT TO ADD UNORDER LISTS : Y/N : ")
+		ini='<body>\n' 
+		lorem='Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nobis doloremque exercitationem eveniet nesciunt at. Voluptates provident vero amet vel omnis excepturi delectus autem accusamus voluptatibus perferendis quasi, debitis quaerat inventore blanditiis eum quis. In quisquam accusantium iusto natus illo possimus ducimus voluptates fugiat, adipisci enim est ullam ut perspiciatis quod officia aliquid praesentium repellat omnis dolor perferendis quia ipsam veritatis commodi reiciendis. Ex, voluptate libero. Non quod recusandae, dicta aliquam reprehenderit saepe cumque minus mollitia eveniet ipsa vitae nesciunt quos fugit! Nisi debitis velit inventore quas obcaecati fugiat doloribus mollitia. Saepe, ratione nostrum aspernatur amet maxime, iusto eius natus aliquid adipisci maiores itaque doloremque quam vero nulla labore cum ipsam non omnis repellat error illo quibusdam nam necessitatibus ipsum. Repellendus eligendi non eum. Quia optio vitae perspiciatis architecto nulla fugit recusandae assumenda illum adipisci eum quasi id, autem dolore quisquam pariatur culpa temporibus maiores ratione nihil. Tempora, quos laborum. Reprehenderit libero exercitationem id! Quidem pariatur nisi blanditiis ipsum ex illum maxime voluptatibus voluptas corrupti dolor maiores placeat, explicabo soluta doloribus itaque exercitationem. Omnis a minus expedita aliquam voluptate odit. A sit vel nisi laudantium enim inventore, maxime commodi adipisci illum natus, voluptate eos dolore atque similique deleniti quia explicabo?'
+		bas=['<!DOCTYPE html>\n','<html lang="en">\n','<head>\n','    <meta charset="UTF-8">\n',
+		'    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n',
+		'    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n',f'    <title>{title_}</title>\n','</head>\n',
+		'<body>\n','</body>\n','</html>']
+		if(p=='Y'):
+			p_(bas,ini)
+			
+		if(img=='Y'):
+			img_(bas,ini)
+			
+		if(H=='Y'):
+			hx_(bas,ini)
+
+		if(sup=='<sup>' or sup=='<sub>'):
+			sup_(bas,ini,sup)
+		if(ol=='Y'):
+			ol_(bas,ini)
+			li_(bas,ini)
+			ini='</ol>\n'
+		if(ul=='Y'):
+			ul_(bas,ini)
+			li_(bas,ini)
+			ini='</ul>\n'
+		
+		f1.writelines(bas)
+main()
+
