770a9d09d8e09dc12d9e2285d976853f104e2674
Manmay <manmaychakarborty@gmail.com>
2023-03-17 11:22:17 +0530

    updated

---
+++ PROJECTS/HTML_CREATOR.py
@@ -0,0 +1,7 @@
+# WELCOME TO THE HTML CREATOR WHICH WILL HELP US TO GENERATE CODE FOR HTML AND CSS
+file_name=input("ENTER THE HTML FILE NAME : ")
+with open(file_name.html,'w') as f1:
+	#basic doctype
+
+	f1.writelines()
+
