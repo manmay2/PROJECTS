23c40421b097661b630eaa066015d968c1e70c78
Manmay <manmaychakarborty@gmail.com>
2023-05-28 12:34:28 +0530

    ADMIT CARD ID GENERATOR

---
+++ PROJECTS/ADMIT_CARD_ID_GENERATOR.py
@@ -0,0 +1,2 @@
+
+print(input("Enter the Name of the student : ")[0]+input("Enter your mother's name : ")[0]+input("Enter ur Roll  :: ")[5]+input("Enter ur Roll  :: ")[6]+input('Enter school code  :  ')[0]+input('Enter school code  :  ')[1]+input("Enter your Centre no.")[2]+input("Enter your Centre no.")[3])
